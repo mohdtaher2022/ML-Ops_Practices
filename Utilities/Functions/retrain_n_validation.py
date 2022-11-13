a057eca8bf0c7eff6171e52f0ad5ece00c815dbc
mohdtaher2022 <116493342+mohdtaher2022@users.noreply.github.com>
2022-11-13 23:39:54 +0530

    Added code for precision recall threshold cutoff

--- Utilities/Functions/retrain_n_validation.py
+++ Utilities/Functions/retrain_n_validation.py
@@ -30,3 +30,48 @@ def sensivity_specificity(y, y_pred):
         'Accuracy': Accuracy
     })
     return df_return
+
+
+def recall_precision_selection(model_, model_location, df, return_graph = True, 
+                               select_metric = 'precision',threshold = 0.4, print_cut_off = True):
+  """
+  model_: Model Name, 
+  model_location: Model Location from the Saved Models, 
+  df: Input Data Frame, 
+  return_graph = True, return Precicion VS recall Graph for Decision Making 
+  select_metric = {'precision' or 'Sensitivity'} as model objective,
+  threshold = 0.4 of selected metric precision or recall, 
+  print_cut_off = True print model cutoff
+  """
+  iter_model = model_names_[model_].load(os.path.join(model_location,model_))
+  test_df_ = iter_model.transform(df)
+  test_df_ = test_df_.withColumn("prob_raw", extract_prob_udf(F.col("probability")))
+  prob = test_df_.select('prob_raw').rdd.flatMap(lambda x: x).collect()
+  actual = test_df_.select('HeartDisease_Yes').rdd.flatMap(lambda x: x).collect()
+  recall_pr = sensivity_specificity(actual, prob)
+  if select_metric == 'precision':
+    selected_threshold_new = recall_pr[(recall_pr['precision'] >= threshold)]
+    max_recall_new = np.max(selected_threshold_new['Sensitivity'])
+    precison_ = threshold
+    recall_ = max_recall_new
+    # Selecting the Threshold Value
+    new_selected_threshold_value = np.max(selected_threshold_new['Cut-Off'][selected_threshold_new['Sensitivity']==max_recall_new])
+  if select_metric == 'Sensitivity':
+    selected_threshold_new = recall_pr[(recall_pr['Sensitivity'] >= threshold)]
+    max_precision_new = np.max(selected_threshold_new['precision'])
+    precison_ = max_precision_new
+    recall_ = threshold
+    # Selecting the Threshold Value
+    new_selected_threshold_value = np.max(selected_threshold_new['Cut-Off'][selected_threshold_new['precision']==max_precision_new])
+  if print_cut_off:
+    print('To attain ',select_metric, ' {0} '.format(threshold),
+          'the {0} Model threshold cut-off has to be set to '.format(model_), new_selected_threshold_value)
+  if return_graph:
+    fig = px.line(recall_pr, x='Cut-Off', y =['Sensitivity', 'precision',])
+    fig.update_layout(showlegend=True, legend=dict(orientation="h"),  plot_bgcolor='white',
+                      autosize=False,width=600,height=400)
+    fig.show()
+  output_obj = {'Model_name':model_,'selected_metric': select_metric, 
+                'precision': np.str(precison_),'recall': np.str(recall_), 
+                'cut-off': np.str(new_selected_threshold_value)}
+  return output_obj
